de773ed1d83342a0b3d06fad98691538060cfc4b
hernandezurbina <hernandezurbina@gmail.com>
2016-11-21 11:01:28 -0800

    Added POS tagging file.

---
+++ POSTagging.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+trainText = state_union.raw("2005-GWBush.txt")
+sampleText = state_union.raw("2006-GWBush.txt")
+
+# train tokenizer:
+customSentTokenizer = PunktSentenceTokenizer(trainText)
+
+tokenized = customSentTokenizer.tokenize(sampleText)
+
+def processContent():
+	try:
+		for i in tokenized:
+			words = nltk.word_tokenize(i)
+			tagged = nltk.pos_tag(words)
+			print(tagged)
+	except Exception as e:
+		print(str(e))
+
+processContent()
+
